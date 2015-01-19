82c07fe80b954afb84d9268c261924cab8dbea98
Phil Baldwin <phil@sheffieldlive.org>
2015-01-19 16:15:51 +0000

    Added file logging and fixed ping

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem "net-ping" # used to send ping requests
 gem "activesupport" # used to strip whitespace/tabs from <<-heredoc
+gem "pry", group: :development
--- device-alert.rb
+++ device-alert.rb
@@ -2,66 +2,117 @@
 # Make it work
 # Seperate classes to make more modular
 # Add database support
+# Add error handling
+# Fix bundler 
+# Add configuration
+
+require "rubygems"
+require "bundler/setup"
+require "pry" # remove for production
+Bundler.require
 
 class DeviceAlert
-  attr_reader :agent
+  attr_reader :agent, :mailer, :hostlist, :devices
   def initialize
-    @agent = Pinger.new
+    @mailer = Mailer.new
+    read_hostlist
+    create_instances_of_devices
   end
 
-  def cycle(devices)
-    devices.each do |device|
-      sleep 5
-      unless @agent.ping(host)
-        Mailer::new::report(device) # Mailer should maybe be a module, why am I create new ones
-        # Maybe I should initialize one then check if exists
+  def read_hostlist
+    @hostlist = []
+    File.open(File.dirname(__FILE__) + '/hostlist') do |file|
+      file.each_line do |line|
+        @hostlist << line.chomp
       end
-      cycle(devices)
     end
   end
-end
-class Pinger
-  require "net/ping" # change for autoload?
-  def initialize
+
+  def create_instances_of_devices
+    @devices = []
+    @hostlist.each do |host|
+      @devices << Device.new(host) 
+    end
   end
 
-  def ping(host)
-    puts Net::Ping::HTTP.new(host) # device.host
+  def cycle(devices) # Need to break this up
+    devices.each do |device|
+      puts "Checking device: #{device.host} on port #{device.port}"
+      sleep 10
+      if device.pinger.ping?
+        puts "#{device.host} responded successfully."
+        device.last_seen = Time.now
+      else
+        puts "Host #{device.host} not found. Logging to file."
+        @mailer.log_to_file(device)
+        # @mailer.report(device) # Mailer should maybe be a module, why am I create new ones
+        # Maybe I should initialize one then check if exists
+        "Logged."
+      end
+    end
+    cycle(devices)
   end
 end
 
 class Device
-  attr_accessor :name, :host, :port, :type, :last_seen
-  def initialize(name, host, port, type)
-    @name = name
+  attr_accessor :host, :port, :type, :pinger, :last_seen
+  def initialize(host, port = 80, type = "camera")
     @host = host
     @port = port
     @type = type
-    @last_seen = Time.now # incorrect but there for testing
+    @pinger = Net::Ping::TCP.new(@host, @port)
+    @last_seen = "Never" 
   end
 end
 
-class Mailer
-  # TODO: move gem requires to Gemfile
-
+class Mailer < DeviceAlert
+  require "active_support/core_ext/string" # Not sure why Bundle.require isn't loading this
+  require "net/smtp"
+  attr_reader :root_path
   def initialize
     @header = <<-EOF.strip_heredoc
-      From: Device Alerter <phil@sheffieldlive.org>
-      To: SLTV Staff <phil@sheffieldlive.org>
+      From: phil@sheffieldlive.org
+      To: phil@sheffieldlive.org
       Subject: Device Not Responding
 
     EOF
+    @root_path = File.expand_path(File.dirname(__FILE__))
+  end
+
+  def log_to_file(device)
+    File.open(logfile, 'a+') do |file|
+      file.puts(report(device))
+    end
   end
 
   def report(device) 
-    "Device: #{device.name} (last seen: #{device.last_seen}) is not responding to ping request."
+    t = Time.now.strftime('%Y/%m/%d %H:%M:%S -')
+    "#{t} Device: #{device.host} (last seen: #{device.last_seen}) is not responding to ping request."
+  end
+
+  def logfile
+    t = Time.now.strftime('%Y%m%d-device-alert.log')
+    @root_path + "/logs/#{t}" # Make configurable
   end
 
   def alert(device) # currently broken as not the correct smtp settings
     message = @header + report(device)
-    Net::SMTP.start('localhost') do |smtp| 
+    Net::SMTP.start('webmail.sheffieldlive.org',
+                    '2095',
+                    'localhost',
+                    ENV['webmail_username'],
+                    ENV['webmail_password'],
+                    :login) do |smtp| 
       smtp.send_message message, "phil@sheffieldlive.org", "phil@sheffieldlive.org"
     end
   end
+end
 
+if ARGV[0] === 'start'
+  app = DeviceAlert.new
+  app.read_hostlist
+  app.create_instances_of_devices
+  puts app.devices
+  puts app.mailer.logfile
+  app.cycle(app.devices)
 end
